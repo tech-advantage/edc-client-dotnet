0f630461df799c20bc57b8ff95a5f793a1776241
bracq <florianbracq42@gmail.com>
2022-07-29 16:31:40 +0200

    Create Injection

---
+++ edc-client-dotnet/Injection/factory/I18NFactory.cs
@@ -0,0 +1,13 @@
+﻿using edc_client_dotnet.internalImpl.model;
+using edc_client_dotnet.model;
+
+namespace edc_client_dotnet.factory
+{
+    public class I18NFactory
+    {
+        public II18NContent Create()
+        {
+            return new I18NContentImpl();
+        }
+    }
+}
